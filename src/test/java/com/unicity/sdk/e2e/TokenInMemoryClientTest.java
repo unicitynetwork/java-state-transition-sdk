282e167f7dbc98d8037554cfb82fb1c8abab543f
Martti Marran <martti.marran@hotmail.com>
2025-09-06 14:14:30 +0400

    Improve the flow in nametag test to represent real world usecase

--- src/test/java/com/unicity/sdk/e2e/TokenInMemoryClientTest.java
+++ src/test/java/com/unicity/sdk/e2e/TokenInMemoryClientTest.java
@@ -5,6 +5,7 @@ import static com.unicity.sdk.utils.TestUtils.randomCoinData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.unicity.sdk.StateTransitionClient;
 import com.unicity.sdk.TestAggregatorClient;
 import com.unicity.sdk.address.Address;
@@ -15,6 +16,7 @@ import com.unicity.sdk.hash.DataHash;
 import com.unicity.sdk.hash.DataHasher;
 import com.unicity.sdk.hash.HashAlgorithm;
 import com.unicity.sdk.predicate.MaskedPredicate;
+import com.unicity.sdk.serializer.UnicityObjectMapper;
 import com.unicity.sdk.signing.SigningService;
 import com.unicity.sdk.token.NameTagTokenState;
 import com.unicity.sdk.token.Token;
@@ -41,8 +43,73 @@ import org.junit.jupiter.api.Test;
  * structure.
  */
 public class TokenInMemoryClientTest {
+  private static final byte[] ALICE_SECRET = "ALICE".getBytes(StandardCharsets.UTF_8);
+  private static final byte[] BOB_SECRET = "ALICE".getBytes(StandardCharsets.UTF_8);
 
-  public static Token<?> mintToken(StateTransitionClient client, byte[] secret) throws Exception {
+  @Test
+  public void testNametagTransfer() throws Exception {
+    StateTransitionClient client = new StateTransitionClient(
+        new TestAggregatorClient());
+
+    // Alice mints token on her environment
+    Token<?> aliceToken = mintToken(client, ALICE_SECRET);
+    assertTrue(aliceToken.verify().isSuccessful());
+
+    // Bob prepares information for receiving the token from alice
+    String bobNameTag = "bob@unicity-labs.com";
+    byte[] bobStateData = "Bob's custom data".getBytes(StandardCharsets.UTF_8);
+
+    // Bob creates predicate for the token he will receive to create an address
+    byte[] bobNonce = randomBytes(32);
+    SigningService bobSigningService = SigningService.createFromSecret(BOB_SECRET, bobNonce);
+    MaskedPredicate bobPredicate = MaskedPredicate.create(bobSigningService, HashAlgorithm.SHA256,
+        bobNonce);
+
+    // Bob creates nametag and put real address into it so alice could use nametag to send token
+    Token<?> bobNametagToken = mintNametagToken(
+        client,
+        BOB_SECRET,
+        bobNameTag,
+        bobPredicate.getReference(aliceToken.getType()).toAddress()
+    );
+
+    // Bob gives nametag and data hash he will put into token to alice and alice transfers token
+    Transaction<TransferTransactionData> aliceToBobTransferTransaction = transferToken(
+        client,
+        ALICE_SECRET,
+        aliceToken,
+        ProxyAddress.create(bobNameTag),
+        new DataHasher(HashAlgorithm.SHA256).update(bobStateData).digest()
+    );
+
+    // Alice creates JSON representations of the token and transaction to simulate sending over network
+    byte[] aliceTokenJson = UnicityObjectMapper.JSON
+        .writeValueAsBytes(aliceToken);
+    byte[] aliceToBobTransferTransactionJson = UnicityObjectMapper.JSON
+        .writeValueAsBytes(aliceToBobTransferTransaction);
+
+    // Bob receives token and transaction in json format and finalizes the transaction
+    Token<?> bobToken = client.finalizeTransaction(
+        UnicityObjectMapper.JSON.readValue(
+            aliceTokenJson,
+            UnicityObjectMapper.JSON.getTypeFactory().constructType(Token.class)
+        ),
+        new TokenState(bobPredicate, bobStateData),
+        UnicityObjectMapper.JSON.readValue(
+            aliceToBobTransferTransactionJson,
+            UnicityObjectMapper.JSON.getTypeFactory().constructParametricType(Transaction.class, TransferTransactionData.class)
+        ),
+        List.of(bobNametagToken)
+    );
+
+    // Verify Bob is now the owner
+    assertTrue(bobToken.verify().isSuccessful());
+    assertTrue(bobToken.getState().getUnlockPredicate().isOwner(bobSigningService.getPublicKey()));
+    assertEquals(aliceToken.getId(), bobToken.getId());
+    assertEquals(aliceToken.getType(), bobToken.getType());
+  }
+
+  private static Token<?> mintToken(StateTransitionClient client, byte[] secret) throws Exception {
     TokenId tokenId = new TokenId(randomBytes(32));
     TokenType tokenType = new TokenType(randomBytes(32));
     TokenCoinData coinData = randomCoinData(2);
@@ -93,7 +160,7 @@ public class TokenInMemoryClientTest {
     );
   }
 
-  public static Token<?> mintNametagToken(StateTransitionClient client, byte[] secret,
+  private static Token<?> mintNametagToken(StateTransitionClient client, byte[] secret,
       String nametag, Address targetAddress) throws Exception {
     TokenType tokenType = new TokenType(randomBytes(32));
     TokenCoinData coinData = randomCoinData(2);
@@ -143,7 +210,7 @@ public class TokenInMemoryClientTest {
     );
   }
 
-  public static Transaction<TransferTransactionData> transferToken(
+  private static Transaction<TransferTransactionData> transferToken(
       StateTransitionClient client,
       byte[] secret,
       Token<?> token,
@@ -177,56 +244,5 @@ public class TokenInMemoryClientTest {
         token,
         inclusionProof
     );
-  }
-
-  @Test
-  public void testNametagTransfer() throws Exception {
-    StateTransitionClient client = new StateTransitionClient(
-        new TestAggregatorClient());
-    byte[] ALICE_SECRET = "ALICE".getBytes(StandardCharsets.UTF_8);
-    byte[] BOB_SECRET = "BOB".getBytes(StandardCharsets.UTF_8);
-
-    // Alice mints a token
-    Token<?> aliceToken = mintToken(client, ALICE_SECRET);
-    assertTrue(aliceToken.verify().isSuccessful());
-
-    // Bob creates nametag
-    String bobNameTag = "bob@unicity-labs.com";
-    byte[] bobStateData = "Bob's custom data".getBytes(StandardCharsets.UTF_8);
-
-    // Alice transfers to Bob
-    Transaction<TransferTransactionData> aliceToBobTransferTransaction = transferToken(
-        client,
-        ALICE_SECRET,
-        aliceToken,
-        ProxyAddress.create(bobNameTag),
-        new DataHasher(HashAlgorithm.SHA256).update(bobStateData).digest()
-    );
-
-    // Bob prepares to receive the token
-
-    byte[] bobNonce = randomBytes(32);
-    SigningService bobSigningService = SigningService.createFromSecret(BOB_SECRET, bobNonce);
-    MaskedPredicate bobPredicate = MaskedPredicate.create(bobSigningService, HashAlgorithm.SHA256,
-        bobNonce);
-
-    // Bob creates nametag
-    Token<?> bobNametagToken = mintNametagToken(client, BOB_SECRET,
-        bobNameTag,
-        bobPredicate.getReference(aliceToken.getType()).toAddress());
-
-    // Bob finalizes the token
-    Token<?> bobToken = client.finalizeTransaction(
-        aliceToken,
-        new TokenState(bobPredicate, bobStateData),
-        aliceToBobTransferTransaction,
-        List.of(bobNametagToken)
-    );
-
-    // Verify Bob is now the owner
-    assertTrue(bobToken.verify().isSuccessful());
-    assertTrue(bobToken.getState().getUnlockPredicate().isOwner(bobSigningService.getPublicKey()));
-    assertEquals(aliceToken.getId(), bobToken.getId());
-    assertEquals(aliceToken.getType(), bobToken.getType());
   }
 }
