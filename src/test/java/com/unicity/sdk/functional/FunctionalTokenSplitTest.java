1276f026e6c144fc72e9be8cd19d5e0f3537b16d
Martti Marran <martti.marran@hotmail.com>
2025-09-06 22:45:44 +0400

    Fix naming of abstract token split class

--- src/test/java/com/unicity/sdk/functional/FunctionalTokenSplitTest.java
+++ src/test/java/com/unicity/sdk/functional/FunctionalTokenSplitTest.java
@@ -2,10 +2,10 @@ package com.unicity.sdk.functional;
 
 import com.unicity.sdk.StateTransitionClient;
 import com.unicity.sdk.TestAggregatorClient;
-import com.unicity.sdk.common.split.TokenSplitTest;
+import com.unicity.sdk.common.split.BaseTokenSplitTest;
 import org.junit.jupiter.api.BeforeEach;
 
-public class FunctionalTokenSplitTest extends TokenSplitTest {
+public class FunctionalTokenSplitTest extends BaseTokenSplitTest {
   @BeforeEach
   void setUp() {
     this.client = new StateTransitionClient(new TestAggregatorClient());
