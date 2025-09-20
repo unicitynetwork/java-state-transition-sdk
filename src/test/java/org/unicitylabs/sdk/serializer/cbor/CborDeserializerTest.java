7053acb3456f00ab8ad2e0ad285e9a4914927194
Martti Marran <martti.marran@hotmail.com>
2025-09-20 22:36:05 +0400

    Added similar to js cbor serializer and deserializer

---
+++ src/test/java/org/unicitylabs/sdk/serializer/cbor/CborDeserializerTest.java
@@ -0,0 +1,174 @@
+package org.unicitylabs.sdk.serializer.cbor;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.unicitylabs.sdk.serializer.cbor.CborDeserializer.CborTag;
+import org.unicitylabs.sdk.serializer.cbor.CborSerializer.CborMap;
+import org.unicitylabs.sdk.serializer.cbor.CborSerializer.CborMap.Entry;
+import org.unicitylabs.sdk.util.HexConverter;
+
+public class CborDeserializerTest {
+
+  @Test
+  void testReadUnsignedInteger() {
+    Assertions.assertEquals(
+        5,
+        CborDeserializer.readUnsignedInteger(HexConverter.decode("05"))
+    );
+
+    Assertions.assertEquals(
+        100,
+        CborDeserializer.readUnsignedInteger(HexConverter.decode("1864"))
+    );
+
+    Assertions.assertEquals(
+        10000,
+        CborDeserializer.readUnsignedInteger(HexConverter.decode("192710"))
+    );
+
+    Assertions.assertEquals(
+        66000,
+        CborDeserializer.readUnsignedInteger(HexConverter.decode("1a000101d0"))
+    );
+
+    Assertions.assertEquals(
+        8147483647L,
+        CborDeserializer.readUnsignedInteger(HexConverter.decode("1b00000001e5a0bbff"))
+    );
+
+    Assertions.assertEquals(
+        -5,
+        CborDeserializer.readUnsignedInteger(HexConverter.decode("1bfffffffffffffffb"))
+    );
+  }
+
+  @Test
+  void testReadByteString() {
+    Assertions.assertArrayEquals(
+        new byte[5],
+        CborDeserializer.readByteString(HexConverter.decode("450000000000"))
+    );
+
+    Assertions.assertArrayEquals(
+        new byte[25],
+        CborDeserializer.readByteString(
+            HexConverter.decode("581900000000000000000000000000000000000000000000000000"))
+    );
+  }
+
+  @Test
+  void testReadTextString() {
+    Assertions.assertEquals(
+        "Hello, world!",
+        CborDeserializer.readTextString(HexConverter.decode("6d48656c6c6f2c20776f726c6421"))
+    );
+
+    Assertions.assertEquals(
+        new String(new byte[25]),
+        CborDeserializer.readTextString(
+            HexConverter.decode("781900000000000000000000000000000000000000000000000000"))
+    );
+  }
+
+  @Test
+  void testReadArray() {
+    List<byte[]> data = CborDeserializer.readArray(
+        HexConverter.decode(
+            "98196d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c64216d48656c6c6f2c20776f726c6421")
+    );
+
+    for (byte[] item : data) {
+      Assertions.assertEquals("Hello, world!", CborDeserializer.readTextString(item));
+    }
+  }
+
+  @Test
+  void testReadMap() {
+    Set<CborMap.Entry> data = CborDeserializer.readMap(
+        HexConverter.decode(
+            "a4430000006d48656c6c6f2c20776f726c6421430000016d48656c6c6f2c20776f726c64216454657374f66d48656c6c6f2c20776f726c6421581900000000000000000000000000000000000000000000000000")
+    );
+
+    Iterator<Entry> iterator = data.iterator();
+    Entry entry = iterator.next();
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeByteString(HexConverter.decode("000000")),
+        entry.getKey()
+    );
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeTextString("Hello, world!"),
+        entry.getValue()
+    );
+
+    entry = iterator.next();
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeByteString(HexConverter.decode("000001")),
+        entry.getKey()
+    );
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeTextString("Hello, world!"),
+        entry.getValue()
+    );
+
+    entry = iterator.next();
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeTextString("Test"),
+        entry.getKey()
+    );
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeNull(),
+        entry.getValue()
+    );
+
+    entry = iterator.next();
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeTextString("Hello, world!"),
+        entry.getKey()
+    );
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeByteString(new byte[25]),
+        entry.getValue()
+    );
+  }
+
+  @Test
+  void testReadBoolean() {
+    Assertions.assertEquals(
+        true,
+        CborDeserializer.readBoolean(HexConverter.decode("f5"))
+    );
+
+    Assertions.assertEquals(
+        false,
+        CborDeserializer.readBoolean(HexConverter.decode("f4"))
+    );
+  }
+
+  @Test
+  void testReadOptional() {
+    Assertions.assertEquals(
+        null,
+        CborDeserializer.readOptional(HexConverter.decode("f6"),
+            CborDeserializer::readUnsignedInteger)
+    );
+  }
+
+  @Test
+  void testEncodeTag() {
+    CborTag tag = CborDeserializer.readTag(
+        HexConverter.decode("d4781a746167206e756d62657220736d616c6c6572207468616e203234")
+    );
+    Assertions.assertEquals(
+        20,
+        tag.getTag()
+    );
+
+    Assertions.assertArrayEquals(
+        CborSerializer.encodeTextString("tag number smaller than 24"),
+        tag.getData()
+    );
+  }
+}
