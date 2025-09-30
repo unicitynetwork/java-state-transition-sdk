d908839ca693955bb075673a1bdb6ceed5c700db
Risto Alas <risto.alas@gmail.com>
2025-09-30 15:03:42 +0300

    Refactor JsonRpcHttpTransport to not depend on a higher level detail like specific HTTP header names

--- src/main/java/org/unicitylabs/sdk/api/AggregatorClient.java
+++ src/main/java/org/unicitylabs/sdk/api/AggregatorClient.java
@@ -1,9 +1,14 @@
 package org.unicitylabs.sdk.api;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import org.unicitylabs.sdk.hash.DataHash;
 import org.unicitylabs.sdk.jsonrpc.JsonRpcHttpTransport;
+
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
 
 public class AggregatorClient implements IAggregatorClient {
 
@@ -26,7 +31,11 @@ public class AggregatorClient implements IAggregatorClient {
 
     SubmitCommitmentRequest request = new SubmitCommitmentRequest(requestId, transactionHash,
         authenticator, false);
-    return this.transport.request("submit_commitment", request, SubmitCommitmentResponse.class, this.apiKey);
+    Map<String, List<String>> headers = new LinkedHashMap<>();
+    if (apiKey != null) {
+        headers.put(AUTHORIZATION, Collections.singletonList("Bearer " + apiKey));
+    }
+    return this.transport.request("submit_commitment", request, SubmitCommitmentResponse.class, headers);
   }
 
   public CompletableFuture<InclusionProofResponse> getInclusionProof(RequestId requestId) {
